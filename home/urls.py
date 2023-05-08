bbd0d3f8791f4588bdadf8f950c187be158326b4
klosebygeo <georgian.ivan95@gmail.com>
2023-05-08 20:23:19 +0300

    first

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+
+from home import views
 
 urlpatterns = [
-
+    path('',views.HomeTemplateView.as_view(), name='homepage')
 ]
--- home/views.py
+++ home/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def home(request):
-    return render(request, 'home/home.html')
+
+class HomeTemplateView(TemplateView):
+    template_name = 'home/homepage.html'
 
