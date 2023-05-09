d403323d512c68199c4370dfe304e7160308f6b3
sidonia05 <sidoniaraluca1994@gmail.com>
2023-05-09 19:54:44 +0300

    1

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class ShopTemplateView(TemplateView):
-    template_name = 'shop/shop.html'
+    template_name = 'shop/list_of_products.html'
