a24b7e385550c9d2e44e6767e04e1ab0c7d776f4
klosebygeo <georgian.ivan95@gmail.com>
2023-05-10 14:32:26 +0300

    customize_more file

---
+++ static/js/my_script.js
@@ -0,0 +1,9 @@
+/*================== toogle icon navbar =================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
