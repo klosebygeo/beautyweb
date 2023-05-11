8515e6cda34123722a46844015424164b8e51eaf
sidonia05 <sidoniaraluca1994@gmail.com>
2023-05-11 18:04:22 +0300

    Shop - accesorii

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,7 @@ from shop import views
 
 urlpatterns = [
     path('shop/',views.ShopTemplateView.as_view(), name='shop'),
+    path('shop/ingrijire_par',views.ShopTemplateView.as_view(), name='shop'),
     path('shop/ingrijire_barba',views.BarbaTemplateView.as_view(), name='barba'),
     path('shop/accesorii',views.AccesoriiTemplateView.as_view(), name='accesorii'),
 ]
