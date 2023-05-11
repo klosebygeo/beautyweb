8f21df43858fb06e6ec8675f74ce31a26f66cf3c
sidonia05 <sidoniaraluca1994@gmail.com>
2023-05-11 20:04:02 +0300

    Shop - sampon

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from .models import Product
 
-admin.site.register(Product)
+# Register your models here.
--- shop/models.py
+++ shop/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
-
 # Create your models here.
-
-PRODUCT_TYPES=[(1,'Ingrijire par'),(2,"Ingrijire barba"),(3,"Accesorii")]
-class Product(models.Model):
-    name = models.CharField(max_length=50, null=False)
-    price = models.FloatField(null=False)
-    quantity = models.IntegerField(null=False)
-    description = models.CharField(max_length=150, null=False)
-    image = models.ImageField(upload_to='uploads/')
-    tip_produs = models.CharField(choices=PRODUCT_TYPES, max_length=20)
-
-    def __str__(self):
-        return self.name
