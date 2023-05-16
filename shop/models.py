371a644fcb63fdf1c4f4065964c31262878bf05d
sidonia05 <sidoniaraluca1994@gmail.com>
2023-05-16 23:13:37 +0300

    Details cart

--- shop/models.py
+++ shop/models.py
@@ -14,10 +14,10 @@ PRODUCT_TYPES = (
 #         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, null=False)
+    name = models.CharField(max_length=80, null=False)
     price = models.FloatField(null=False)
     quantity = models.IntegerField(null=False)
-    description = models.CharField(max_length=150, null=False)
+    description = models.TextField(max_length=650, null=False)
     image = models.ImageField(upload_to='uploads/')
     tip_produs = models.CharField(choices=PRODUCT_TYPES, max_length=20)
     um = models.CharField(max_length=10, null=True)
