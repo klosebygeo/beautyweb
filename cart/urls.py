c2d2a109856cf71bcdf23ee0e4939c97a948267e
IsabellaElena27 <ysss_a@yahoo.com>
2023-05-18 23:07:32 +0300

    commit changes

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from .views import RegisterProduct, AllProducts
 
 urlpatterns = [
-     path('addcart/<int:user_id>/<int:id_p>/', RegisterProduct.as_view(), name='addcart'),
+     path('addcart/<int:user>/<int:id_p>/', RegisterProduct.as_view(), name='addcart'),
      path('', AllProducts.as_view(), name='get-cart-user'),
+
 ]
