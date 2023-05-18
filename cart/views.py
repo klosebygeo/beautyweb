7584993c1b3b01830d4d9fda4a527e87cf34fb13
sidonia05 <sidoniaraluca1994@gmail.com>
2023-05-18 20:10:09 +0300

    cos

--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,13 @@
 from django.views.generic import CreateView, ListView
 from .models import Cart
 
-
-# Create your views here.
-
 class RegisterProduct(CreateView):
     model = Cart
     template_name = 'addcart/addcart.html'
 
-
-class AllProducts(ListView):  # se foloseste si pentru toate produsele neachitate si pt
-    # produse dintr-un anumit cart
+class AllProducts(ListView):
     template_name = 'addcart/addcart.html'
 
     def get_queryset(self):
-        return Cart.objects.filter(id_user=user_id, status=0)
+        user_id = self.request.user.id
+        return Cart.objects.filter(id_user=user_id, status=False)
