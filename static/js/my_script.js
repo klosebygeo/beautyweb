9ce2e307e9650adae5aa5057cb5b55fe97820219
klosebygeo <georgian.ivan95@gmail.com>
2023-05-19 22:32:49 +0300

    commit multiple css

--- static/js/my_script.js
+++ static/js/my_script.js
@@ -34,56 +34,63 @@ const product = [
   }
 ];
 
-const addtocart = (id) => {
-  // Implementați funcționalitatea adăugării produsului în coșul de cumpărături aici
-  console.log(`Produsul cu id-ul ${id} a fost adăugat în coșul de cumpărături.`);
-}
-
 const categories = [...new Set(product.map((item) =>
-{return item}))];
-let i = 0;
-document.getElementById("root").innerHTML = categories.map((item) => {
+    {return item}))]
+    let i = 0;
+document.getElementById('root').innerHTML = categories.map((item) =>
+{
   var { image, title, price } = item;
   return (
     `<div class='box'>
       <div class='img-box'>
         <img class='images' src=${image}></img>
-      </div>
+              </div>
       <div class='bottom'>
         <p>${title}</p>
-        <h2>$ ${price}.00</h2>`+
-        "<button onclick='addtoCart("+(i++)+")'>Add to cart</button>"+
-      `</div>
+        <h2>$ ${price}.00</h2>` +
+    "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
+    `</div>
     </div>`
-  );
-}).join('');
+  )
+}).join('')
 
 var cart =[];
 
-function  addcart(a){
+function addtocart(a){
     cart.push({...categories[a]});
     displaycart();
 }
+function delElement(a){
+    cart.splice(a,1);
+    displaycart();
+}
 
-
-function  displaycart(a){
-    let j = 0;
-    if(cart.length==0){
+function displaycart(a) {
+    let j = 0, total = 0;
+    document.getElementById("count").innerHTML = cart.length;
+    if (cart.length == 0) {
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
-    }
-    else{
-        document.getElementById("cartItem").innerHTML = cart.map((items)=>
-        {
+        document.getElementById("total").innerHTML = "$ " + 0 + ".00";
+    } else {
+        document.getElementById("cartItem").innerHTML = cart.map((items) => {
             var {image, title, price} = items;
-            return(
+            total = total + price;
+            document.getElementById("total").innerHTML = "$ " + total + ".00";
+            return (
                 `<div class='cart-item'>
                     <div class='row-img'>
-                        <img class='rowing' src=${image}>
+                        <img class='rowimg' src=${image}>
                     </div>
                     <p style='font-size:12px;'>${title}</p>
-                    <h2 style='font-size:15px;'>$ ${price}.00</h2>`+
-                    "<i class='fa-solid fa-trash' onclick='delElement("+(j++)+")'></i></div>"
+                    <h2 style='font-size:15px;'>$ ${price}.00</h2>` +
+                "<i class='fa-solid fa-trash' onclick='delElement(" + (j++) + ")'></i></div>"
             );
         }).join('');
     }
 }
+
+
+
+
+
+
