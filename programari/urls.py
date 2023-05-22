be12c0577804135c3775151c6b069829ac7c3e55
IsabellaElena27 <ysss_a@yahoo.com>
2023-05-22 17:50:21 +0300

    commit rezervare programare2

--- programari/urls.py
+++ programari/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from programari import views
-from programari.views import RezervareProgramareView
 
 urlpatterns = [
     path('creare_serviciu/', views.ServiceCreateView.as_view(), name='creare-serviciu'),
@@ -10,6 +9,6 @@ urlpatterns = [
     path('delete_service/<int:pk>', views.ServiceDeleteView.as_view(), name='delete-service'),
     path('details_service/<int:pk>', views.ServiceDetailView.as_view(), name='details-service'),
     path('rezervare/', views.Rezervare.as_view(), name='rezervare'),
-    path('rezervare/programare/', RezervareProgramareView.as_view(), name='rezervare-programare'),
+    path('rezervare/programare/', views.RezervareProgramareView.as_view(), name='rezervare-programare'),
 
 ]
